1389711d47af02e076580001b9aa68b14bf66c91
pavelsof <mail@pavelsof.com>
2020-11-07 22:27:28 +0100

    An inline admin for the sample project.

--- sample_project/echoes/admin.py
+++ sample_project/echoes/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
 from django_mirror.admin import MirrorAdmin
 
-from echoes.models import Echo
+from echoes.models import Echo, Utterance
 
 
-@admin.register(Echo)
-class EchoAdmin(MirrorAdmin, admin.ModelAdmin):
+@admin.register(Utterance)
+class UtteranceAdmin(MirrorAdmin, admin.ModelAdmin):
     mirror_fields = (
         ('words', {
-            'mode': 'markdown',
+            'mode': 'rst',
             'line_wrapping': True,
         }),
     )
+
+    class EchoAdmin(MirrorAdmin, admin.StackedInline):
+        model = Echo
+        extra = 0
+        mirror_fields = (
+            ('words', {
+                'mode': 'markdown',
+                'line_wrapping': True
+            }),
+        )
+
+    inlines = (EchoAdmin,)
--- sample_project/echoes/models.py
+++ sample_project/echoes/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 
+class Utterance(models.Model):
+    words = models.TextField(help_text='Use reStructuredText here.')
+
+
 class Echo(models.Model):
-    words = models.TextField()
+    utterance = models.ForeignKey(Utterance, on_delete=models.CASCADE)
+    words = models.TextField(help_text='Uses markdown here.')
 
     class Meta:
         verbose_name_plural = 'echoes'
